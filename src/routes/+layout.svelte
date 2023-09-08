7069b2b2d9ee6c1b7a511d628a6ebe1b924f6bad
Dipun Mistry <dipunm@gmail.com>
2023-09-08 22:25:40 +0100

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    export const ssr = false;
+</script>
+
+<!DOCTYPE html />
+<html lang="en-GB">
+    <head>
+        <title>Hello Worlds</title>
+    </head>
+    <body>
+        <slot></slot>
+    </body>
+</html>
