03d853f359d794f8be4844fcc9614b3cc11cdebe
Dipun Mistry <dipunm@gmail.com>
2023-10-26 11:22:19 +0100

    rebuilt

--- docs/_app/immutable/chunks/navigation.0bc929be.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./singletons.1837933f.js";const a=o("goto"),n=o("on_navigate");export{a as g,n as o};
---
+++ docs/_app/immutable/chunks/navigation.b8a664bb.js
@@ -0,0 +1,1 @@
+import{k as o}from"./singletons.0b1c1643.js";const a=o("goto"),n=o("on_navigate");export{a as g,n as o};
