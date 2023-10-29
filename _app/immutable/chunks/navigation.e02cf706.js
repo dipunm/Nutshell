411e7f51082d9108643176e06fb49eec81bbc173
dipunm <dipunm@gmail.com>
2023-10-29 16:58:41 +0000

    Deploying to gh-pages from @ dipunm/Nutshell@247602c00ca5f0a7aed62867a1dcc8f5de3f6192 🚀

---
+++ _app/immutable/chunks/navigation.e02cf706.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.23cc21b2.js";const a=o("goto"),n=o("on_navigate");export{a as g,n as o};
--- _app/immutable/chunks/navigation.e6933770.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.f2376cf4.js";const a=o("goto"),n=o("on_navigate");export{a as g,n as o};
