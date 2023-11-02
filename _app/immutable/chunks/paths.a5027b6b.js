504adc900c95b6d76b49064f784fac11bf03472c
dipunm <dipunm@gmail.com>
2023-11-02 00:43:51 +0000

    Deploying to gh-pages from @ dipunm/Nutshell@d21c818378a40000fac1bd1b910dd40b9681b471 🚀

---
+++ _app/immutable/chunks/paths.a5027b6b.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1wdxtel)==null?void 0:s.base)??"/Nutshell";var e;const l=((e=globalThis.__sveltekit_1wdxtel)==null?void 0:e.assets)??t;export{l as a,t as b};
